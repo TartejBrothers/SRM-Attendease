f393daec8ae705174b1d76d699275aa366d22979
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-01-09 21:37:45 +0530

    Updated Project page

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import "./styles/project.css";
 import "./styles/global.css";
+import "./styles/project.css";
 export default function ProjectPage() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(null);
@@ -14,27 +14,40 @@ export default function ProjectPage() {
       </h1>
       <div className="projectbody">
         <h5>Pick a single date or add a range</h5>
-        <div className="dateform">
-          <DatePicker
-            wrapperClassName="datePicker"
-            placeholderText="Select Start Date"
-            selected={startDate}
-            selectsStart
-            startDate={startDate}
-            endDate={endDate}
-            onChange={(date) => setStartDate(date)}
-          />
-          <DatePicker
-            wrapperClassName="datePicker"
-            selected={endDate}
-            selectsEnd
-            startDate={startDate}
-            placeholderText="Select End Date"
-            endDate={endDate}
-            minDate={startDate}
-            onChange={(date) => setEndDate(date)}
-          />
+        <form>
+          <div className="dateform">
+            <DatePicker
+              wrapperClassName="datePicker"
+              placeholderText="Select Start Date"
+              selected={startDate}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              onChange={(date) => setStartDate(date)}
+            />
+            <DatePicker
+              wrapperClassName="datePicker"
+              selected={endDate}
+              selectsEnd
+              startDate={startDate}
+              placeholderText="Select End Date"
+              endDate={endDate}
+              minDate={startDate}
+              onChange={(date) => setEndDate(date)}
+            />
+            <button type="submit" className="submitbutton">
+              Add To Records
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="datesadded">
+        <div className="datemain">
+          <h5>Dates Added</h5>
         </div>
+        <div className="datebody">15th November</div>
+        <div className="datebody">18th November</div>
+        <div className="datebody">22nd - 30th November </div>
       </div>
     </div>
   );
