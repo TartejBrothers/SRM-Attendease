5d4eab60e0fb2335ad682105ceac07594fa66f43
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-01-09 13:13:41 +0530

    Project page added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import HomePage from "./components/home";
+import ProjectPage from "./components/project";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/project" element={<ProjectPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
