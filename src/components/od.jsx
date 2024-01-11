1fbbd5b24c660ca1a9d97bcc2687da999fad2b0f
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-01-11 22:47:05 +0530

    Od Page Done

--- src/components/od.jsx
+++ src/components/od.jsx
@@ -9,12 +9,9 @@ export default function OdPage() {
         SRM ATTEND<font color="#00ADB5">EASE</font>
       </h1>
       <div className="odbody">
-        <p>Your Attendance With OD’s</p>
+        <p>OD/ML Taken</p>
       </div>
       <div className="odmaintop">
-        <div className="odheader">
-          <h5>OD’s Taken</h5>
-        </div>
         <div className="odcontentbody">
           <p>15th November</p>
         </div>
@@ -27,19 +24,29 @@ export default function OdPage() {
       </div>
       <div className="odmainbottom">
         <div className="odheader">
-          <h5>Attendance with OD</h5>
+          <p>Attendance With OD/ML</p>
         </div>
-        <div className="odcontentbodybottom">
-          <div className="bottomcontent">Advanced Calculus</div>
-          <div className="bottomcontent">87.4%</div>
-        </div>
-        <div className="odcontentbodybottom">
-          <div className="bottomcontent">OOPS</div>
-          <div className="bottomcontent">91.2%</div>
-        </div>
-        <div className="odcontentbodybottom">
-          <div className="bottomcontent">EGD</div>
-          <div className="bottomcontent">90.5%</div>
+        <div className="attendancerecords">
+          <div className="attendanceheader">
+            <div className="attendancecolumns">Subject</div>
+            <div className="attendancecolumns">Current Attendance</div>
+            <div className="attendancecolumns">New Attendance</div>
+          </div>
+          <div className="attendancerows">
+            <div className="attendancecolumns">Mathematics</div>
+            <div className="attendancecolumns">72.5%</div>
+            <div className="attendancecolumns">78%</div>
+          </div>
+          <div className="attendancerows">
+            <div className="attendancecolumns">Chemistry</div>
+            <div className="attendancecolumns">85%</div>
+            <div className="attendancecolumns">89%</div>
+          </div>
+          <div className="attendancerows">
+            <div className="attendancecolumns">Physics</div>
+            <div className="attendancecolumns">67%</div>
+            <div className="attendancecolumns">72%</div>
+          </div>
         </div>
       </div>
     </div>
